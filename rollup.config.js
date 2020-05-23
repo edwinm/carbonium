2c32ab099d417113925aae10c2a37df925d47596
Edwin Martin <edwin@bitstorm.org>
2020-05-23 20:57:22 +0200

    First commit

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from '@rollup/plugin-typescript';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/bundle.min.js',
+    format: 'iife',
+    name: 'bundle',
+  },
+  plugins: [typescript(), terser(), livereload()],
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Playground');
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('out').textContent = "Done.";
+});
+
