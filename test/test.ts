bbb52eb6888a6a1689f6016921db3b8e11300aac
Edwin Martin <edwin@bitstorm.org>
2020-05-27 23:31:23 +0200

    Add beforeEach to test

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,18 @@
 import * as assert from 'assert';
+import { $ } from '../src/miq';
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+describe('$', () => {
+  beforeEach(() => {
+    document.body.textContent = '';
+    for (let i = 0; i < 6; i++) {
+      const div = document.createElement('div');
+      div.textContent = `item${i}`;
+      document.body.appendChild(div);
+    }
+  });
+
+  it('textContent', () => {
+    $('div:first-child').textContent = 'hello';
+    assert.equal($('div:first-child').textContent, 'hello');
   });
 });
