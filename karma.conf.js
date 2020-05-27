8c6681a929e7b0ca52c770919c7e5976bfa98150
Edwin Martin <edwin@bitstorm.org>
2020-05-27 23:31:07 +0200

    Simplify karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,16 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'chai', "karma-typescript"],
-    files: ["test/*.ts"],
+    files: ["test/*.ts", "src/*.ts"],
     preprocessors: {
-      "**/*.ts": "karma-typescript" // *.tsx for React Jsx
+      "**/*.ts": "karma-typescript"
     },
     reporters: ['progress', "karma-typescript"],
-    port: 9876,  // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
     browsers: ['ChromeHeadless'],
     autoWatch: false,
     singleRun: true,
-    // singleRun: false, // Karma captures browsers, runs the tests and exits
     concurrency: Infinity
   })
 }
