48cc8988ea2acc7ca3e22c8b2895da93b46f282c
Edwin Martin <edwin@bitstorm.org>
2020-05-28 11:50:02 +0200

    Failing unit test in headless Chrome

--- test/test.ts
+++ test/test.ts
@@ -11,8 +11,29 @@ describe('$', () => {
     }
   });
 
-  it('textContent', () => {
+  it('textContent one element', () => {
     $('div:first-child').textContent = 'hello';
-    assert.equal($('div:first-child').textContent, 'hello');
+    const divs = document.getElementsByTagName('div');
+    assert.equal(divs[0].textContent, 'hello');
+  });
+
+  it('textContent all elements', () => {
+    $('div').textContent = 'hello';
+    assert.equal(document.body.textContent, 'hellohellohellohellohellohello');
+  });
+
+  // it('setAttribute all elements', () => {
+  //   $('div').setAttribute('aria-label', 'List item');
+  //   const divs = document.getElementsByTagName('div');
+  //   assert.equal(divs[0].getAttribute('aria-label'), 'List item');
+  //   assert.equal(divs[1].getAttribute('aria-label'), 'List item');
+  //   assert.equal(divs[5].getAttribute('aria-label'), 'List item');
+  // });
+
+  it('filter', () => {
+    $('div')
+      .filter((el) => el.textContent == 'item1')
+      .textContent = 'hello';
+    assert.equal(document.body.textContent, 'hellohellohellohellohellohello');
   });
 });
