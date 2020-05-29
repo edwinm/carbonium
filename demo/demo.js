cc9fde366cd1a8adc0b34e8ccb0b52df5de956b1
Edwin Martin <edwin@bitstorm.org>
2020-05-30 00:49:23 +0200

    Create module, add demo page

---
+++ demo/demo.js
@@ -0,0 +1,5 @@
+import {$} from '../dist/bundle.min.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('#out').innerText = "Hello. It is working!";
+});
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
 import typescript from '@rollup/plugin-typescript';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/miq.ts',
   output: {
     file: 'dist/bundle.min.js',
-    format: 'iife',
+    format: 'es',
     name: 'bundle',
   },
-  plugins: [livereload(), typescript(), terser()],
+  plugins: [typescript(), terser()],
 };
