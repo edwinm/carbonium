bd72da8712204698a4e91fc51ceb3b462d96d22d
Edwin Martin <edwin@bitstorm.org>
2020-05-29 20:12:05 +0200

    Better typing

--- src/miq.ts
+++ src/miq.ts
@@ -1,10 +1,10 @@
-// Used by classList
-let classListNodelist: NodeListOf<Element>;
-
 export function $(arg: string, doc?: Document): any {
   const nodelist = (doc || document).querySelectorAll(arg);
   return new Proxy(nodelist, proxyHandler);
 }
+
+// Used by classList
+let classListNodelist: NodeListOf<Element>;
 
 const proxyHandler: ProxyHandler<NodeListOf<Element>> = {
   get(target, prop) {
@@ -20,7 +20,7 @@ const proxyHandler: ProxyHandler<NodeListOf<Element>> = {
       const propValue = Reflect.get(document.body.classList, prop);
 
       if (typeof propValue == 'function') {
-        return new Proxy(propValue, {
+        return new Proxy<Function>(propValue, {
           apply: function (target, thisArg, argumentsList) {
             classListNodelist.forEach((el) => {
               Reflect.apply(target, el.classList, argumentsList);
@@ -37,27 +37,29 @@ const proxyHandler: ProxyHandler<NodeListOf<Element>> = {
     if (Array.prototype.hasOwnProperty(prop)) {
       const propValue = Reflect.get(Array.prototype, prop);
       if (typeof propValue == 'function') {
-        return new Proxy(propValue, proxyHandler);
+        return new Proxy(propValue, proxyFunctionHandler);
       }
     }
 
     // Get property or call function on DOM elements
     if (target.length > 0) {
-      // Might be DOM element specific, like input.select(), so use first array element to get reference
+      // Might be DOM element specific, like input.select(),
+      // so use first array element to get reference
       if (prop in target[0]) {
         const propValue = Reflect.get(target[0], prop);
         if (typeof propValue == 'function') {
-          return new Proxy(propValue, proxyHandler);
+          return new Proxy(propValue, proxyFunctionHandler);
         } else {
           return propValue;
         }
       }
     } else {
-      // Empty list, targeted DOM element unknown, use HTMLElement and document.body
+      // Empty list, targeted DOM element unknown,
+      // use HTMLElement and document.body
       if (prop in document.body) {
         const propValue = Reflect.get(document.body, prop);
         if (typeof propValue == 'function') {
-          return new Proxy(propValue, proxyHandler);
+          return new Proxy(propValue, proxyFunctionHandler);
         } else {
           return propValue;
         }
@@ -74,22 +76,23 @@ const proxyHandler: ProxyHandler<NodeListOf<Element>> = {
     });
     return true;
   },
+};
 
+const proxyFunctionHandler: ProxyHandler<Function> = {
   apply: function (target, thisArg, argumentsList) {
-    const targetFn = <Function><any>target;
-    if (Array.prototype.hasOwnProperty(targetFn.name)) {
-      const ret = Reflect.apply(targetFn, thisArg, argumentsList);
+    if (Array.prototype.hasOwnProperty(target.name)) {
+      const ret = Reflect.apply(target, thisArg, argumentsList);
       // forEach returns same array
       const newTarget = typeof ret != 'undefined' ? ret : thisArg
       return new Proxy(newTarget, proxyHandler);
     } else {
       // Apply on individual elements
       for (const el of thisArg) {
-        Reflect.apply(targetFn, el, argumentsList);
+        Reflect.apply(target, el, argumentsList);
       }
       return thisArg;
     }
   },
-};
+}
 
 
