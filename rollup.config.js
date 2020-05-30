2e4c0e0d2114a40e1abd300abe01a56b7fc5a57d
Edwin Martin <edwin@bitstorm.org>
2020-05-30 10:27:26 +0200

    Generate module and commonjs versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import typescript from '@rollup/plugin-typescript';
-import { terser } from "rollup-plugin-terser";
+import {terser} from "rollup-plugin-terser";
 
 export default {
   input: 'src/miq.ts',
-  output: {
+  output: [{
     file: 'dist/bundle.min.js',
     format: 'es',
     name: 'bundle',
-  },
+  }, {
+    file: 'dist/bundle.common.min.js',
+    format: 'cjs',
+    name: 'bundle',
+  }],
   plugins: [typescript(), terser()],
 };
